4eadea8399d6bff8a78a6db862ed6f785f8b295b
chrislikescisco <cheesygodlikerobot@gmail.com>
2024-04-16 22:10:23 -0400

    Added more attributes to player class

--- basedball.h
+++ basedball.h
@@ -25,7 +25,39 @@ class Basedball { // I am thinking UpperCamelCase classes, lowerCamelCase funcs,
         char bats;
         char throws;
         Player();
+        int games;
+        // Hitting stats:
+        int at_bats;
+        int runs;
+        int hits;
+        int doubles;
+        int triples;
+        int home_runs;
+        int rbi;
+        int stolen_bases;
+        int caught_stealing;
+        int batted_balls;
+        int strikeouts;
+        int intentional_walks;
+        int hit_by_pitch;
+        int sac_bunts;
+        int sac_flies;
+        int groundouts;
+        double batting_avg; // not included in dataset but can be calculated
+        
 
+        // Fielding stats:
+         // Moved to main player class because all players have these
+        int games_started;
+        int inning_outs;
+        int putouts;
+        int assists;
+        int errors;
+        int double_plays;
+        int passed_balls;
+        int stolen_bases_allowed;
+        int caught_stealing_inf;
+        int zone_rating;
     };
     class Pitcher : public Player { // yeah, we're using inheritance. so sorry :(
         int wins;
@@ -36,29 +68,29 @@ class Basedball { // I am thinking UpperCamelCase classes, lowerCamelCase funcs,
         int shutouts;
         int saves;
         int ip_outs;
-        int hits_given;
-        int home_runs;
-        int strike_outs;
-        int batted_balls;
+        int hits_allowed;
+        int hr_allowed;
+        int so_issued;
+        int bb_allowed;
         int ba_opp;
         double era;
-        int intentional_walks;
+        int iw_issued;
         int wild_pitches;
-        int hit_by_pitch;
+        int hbp_issued;
         int balks;
         int batters_facing_pitcher;
         int games_finished;
         int runs_given;
-        int sac_bunts;
-        int sac_flies;
+        int sh_issued;
+        int sf_issued;
         // there's one more stat for pitchers: GIDP, but this has a super low
         // sample size and is honestly not even specific to pitchers, fielding
         // also factors into it so i chose not to include it.
     };
 
-    class infielder : public Player {
-
-    };
     Basedball();
-
+    // we will include all players in a container of some kind, either an array
+    // or a set. the array would allow us to sort in-place with heapsort but we
+    // would have to iterate through it to search for playerIDs when adding stats
+    // from different CSVs (mainly an issue with pitching).
 };
