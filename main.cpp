cfa0a458088f2e715c2bed2d0fd7828aa2426ce2
chrislikescisco <cheesygodlikerobot@gmail.com>
2024-04-21 19:38:29 -0400

    I think it is finally done please please please please

--- main.cpp
+++ main.cpp
@@ -4,4 +4,14 @@
 int main() {
     Basedball based;
     based.read();
+    cout << "        ____________\n"
+            "           /    __     /\n"
+            "          /    /__/   /\n"
+            "         /           /\n"
+            "        /           \\\n"
+            "       /             \\\n"
+            "      |              |\n"
+            "      /______________/"
+    << endl;
+    return 0;
 }
